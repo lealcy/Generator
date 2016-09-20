a1f46a59ad0bdd50c2061bf2cb550c6b81fa7a25
Lealcy Tardelli Belegante <lealcy@gmail.com>
2016-09-20 01:04:01 -0300

    Initial Commit

---
+++ Generator/Generator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generator
+{
+    class Generator
+    {
+        public int[] Target;
+        public Random RandomSource;
+
+        public Generator(int[] target, Random randomSource)
+        {
+            Target = target;
+            RandomSource = randomSource;
+        }
+
+        public Generator(string target, Random randomSource): 
+            this(Generator.TargetFromString(target), randomSource)
+        { }
+
+        public static int[] TargetFromString(string text)
+        {
+            int[] target = new int[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                target[i] = (int)text[i];
+            }
+
+            return target;
+        }
+    }
+}
--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generator
 {
@@ -10,6 +6,7 @@ namespace Generator
     {
         static void Main(string[] args)
         {
+            Generator g = new Generator("Hello World!", new Random());
         }
     }
 }
